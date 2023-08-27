bbbcf332fc61e9df69ade3d7cf2944db7a5f29cf
rachemelendres <rache.melendres@indebted.co>
2023-08-27 15:48:54 +0800

    refactored WeatherAPIConn class

--- src/utils/api_conn.py
+++ src/utils/api_conn.py
@@ -1,47 +1,55 @@
 import requests
-from requests import Session
 import os
 from dotenv import find_dotenv, load_dotenv
-import json
 import logging
 
 logger = logging.getLogger(__name__)
 
 load_dotenv(find_dotenv())
 
-API_KEY = os.getenv('API_KEY')
-
-class APIConnection:
-    def __init__(self, key=API_KEY):
-        self.apiurl = 'http://dataservice.accuweather.com/forecasts/v1'
-        self.apikey = key
+class WeatherAPIConn:
+    def __init__(self, API_KEY=os.getenv('API_KEY'), API_URL=os.getenv('API_URL'),location='264879'):
+        self.apiurl = API_URL,
+        self.apikey = API_KEY,
+        self.location = location,
         self.params = {'apikey': self.apikey}
-        self.session = Session()
+        self.session = requests.Session()
 
-    def getDailyForecast(self) -> dict:
+    def get_daily_forecast(self) -> dict:
         """
         Retrieves the daily forecast from the API.
 
-        :param self: The instance of the class.
         :return: A dictionary containing the daily forecast.
         :rtype: dict
         """
-        logger.info('Getting daily forecast...')
-        url = f'{self.apiurl}/daily/5day/264879/'
-        response = self.session.get(url, params=self.params)
-        logger.info('Daily forecast retrieved.')
-        return response.json()
+        try:
+            logger.info('Getting daily forecast...')
+            url = f'{self.apiurl[0]}/daily/5day/{self.location[0]}/'
+            response = requests.get(url, params=self.params)
+            response.raise_for_status()
+            logger.info('Daily forecast retrieved.')
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            logger.error(f'Error retrieving daily forecast: {e}')
+            raise e
 
-    def getHourlyForecast(self) -> dict:
+
+
+    def get_hourly_forecast(self) -> dict:
         """
         Retrieves the hourly forecast from the API.
 
         :return: A dictionary containing the hourly forecast data.
         :rtype: dict
         """
-        logger.info('Getting hourly forecast...')
-        url = f'{self.apiurl}/hourly/12hour/264879/'
-        response = self.session.get(url, params=self.params)
-        logger.info('Hourly forecast retrieved.')
-        return response.json()
+        try:
+            logger.info('Getting hourly forecast...')
+            url = f'{self.apiurl[0]}/hourly/12hour/{self.location[0]}/'
+            response = requests.get(url, params=self.params)
+            response.raise_for_status()
+            logger.info('Hourly forecast retrieved.')
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            logger.error(f'Error retrieving hourly forecast: {e}')
+            raise e
 
